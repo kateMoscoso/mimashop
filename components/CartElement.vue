141695a2839396be860ebcafdcff3a0f07323d4d
kateMoscoso <katiemscs@gmail.com>
2020-03-08 18:30:01 +0100

    cart

--- components/CartElement.vue
+++ components/CartElement.vue
@@ -1,37 +1,31 @@
 <template>
-  <v-list-item>
-    <v-avatar
-      class="ma-3"
-      size="125"
-      tile
-    >
-      <v-img :src="product.element.image_url"></v-img>
-    </v-avatar>
-    <v-list-item-content>
-      <v-list-item-title v-text="product.element.productName"></v-list-item-title>
-    </v-list-item-content>
-    <v-text-field
-      :value="product.amount"
-      outlined
-      clearable
-      disabled
-    >
-      <template v-slot:prepend>
-        <v-btn icon @click="() => $store.dispatch('addCart', product.element)">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </template>
-      <template v-slot:append-outer>
-        <v-btn icon @click="changeFavorite">
-          <v-icon>mdi-minus</v-icon>
-        </v-btn>
-      </template>
-    </v-text-field>
-    <v-spacer></v-spacer>
-    <v-list-item-action>
-      <v-list-item-action-text v-text="product.element.price"></v-list-item-action-text>
-    </v-list-item-action>
-  </v-list-item>
+  <div>
+    <v-list-item>
+      <v-avatar
+        class="ma-3"
+        tile
+      >
+        <v-img :src="product.element.image_url"></v-img>
+      </v-avatar>
+      <v-list-item-content class="ma-3">
+        <v-list-item-title v-text="product.element.productName"></v-list-item-title>
+        <div>
+          <v-btn text class="d-inline pa-2  ">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+          <div class="d-inline pa-2 accent-4 ">
+            0
+          </div>
+          <v-btn text class="d-inline pa-2  ">
+            <v-icon>mdi-minus</v-icon>
+          </v-btn>
+        </div>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-list-item-action-text v-text="`${product.element.price} €`"></v-list-item-action-text>
+      </v-list-item-action>
+    </v-list-item>
+  </div>
 </template>
 
 <script>
@@ -53,7 +47,9 @@ export default {
 
   },
   methods: {
-
+    removeElement () {
+      console.log('remove')
+    }
   }
 }
 </script>
--- store/state.js
+++ store/state.js
@@ -1,4 +1,6 @@
 export default () => ({
-  cart: {},
+  cart: {
+
+  },
   amount: 0
 })
